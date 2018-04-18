43c2fcceda5bf1ebc9a5604f977887a7196cede4
Taj Huque <tajulhuque@gmail.com>
2018-04-17 20:57:41 -0400

    message text added on button

--- src/app/joke.ts
+++ src/app/joke.ts
@@ -3,10 +3,24 @@ export class Joke {
   setup: string;
   punchline: string;
   hide: boolean;
+  msgText: string;
 
   constructor(setup: string, punchline: string) {
     this.setup = setup;
     this.punchline = punchline;
     this.hide = true;
+    this.msgText = 'Show Me!';
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+
+    // this.msgText = this.hide ? 'Show Me!' : 'Hide'
+
+    if (this.hide) {
+      this.msgText = 'Show Me!';
+    } else {
+      this.msgText = 'Hide';
+    }
   }
 }
